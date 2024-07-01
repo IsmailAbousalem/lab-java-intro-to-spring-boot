0d98575ce01a403ada88b387e601b33e5af8a610
Ismail <ia.abousalem@gmail.com>
2024-07-01 14:47:12 -0400

    created spring application, added GitHub repo, and added hospital to the mysql datasource

---
+++ src/main/java/com/IntroToSpring/Lab402IntroToSpringBoot/Lab402IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.IntroToSpring.Lab402IntroToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab402IntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab402IntroToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IntroToSpring/Lab402IntroToSpringBoot/Lab402IntroToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntroToSpring.Lab402IntroToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab402IntroToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
